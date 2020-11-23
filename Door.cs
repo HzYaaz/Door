a74a771f332ff5d26cbe73609ef307180d888b5f
Yaaz <yagizcemc@gmail.com>
2020-11-23 16:32:11 +0300

    Update Door.cs

--- Door.cs
+++ Door.cs
@@ -1,37 +1,53 @@
 using UnityEngine;
 using System.Collections;
 
-public class Door : MonoBehaviour {
+public class Door : MonoBehaviour
+{
 
-	[SerializeField]private float distance;
-	[SerializeField]private GameObject player;
+	[SerializeField] private float distance;
+	[SerializeField] private GameObject player;
 	private Ray ray;
-	[SerializeField]private bool opened = false;
-	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player");
+	private bool dondur = true;
+	[SerializeField] private bool opened = false;
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 
-	void Update () {
+	void Update()
+	{
 
 		if (opened == false)
-		{
-		distance = Vector3.Distance(transform.position, player.transform.position);
-		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-			if (Input.GetKeyDown(KeyCode.Mouse0) && distance < 2 && Physics.Raycast (ray, out hit) && hit.collider.gameObject.tag == "Door") {
-				gameObject.GetComponent<Animation>().Play ("DoorOpen");
-				opened = true;
-			}
-		}
-		else
-		{
+        {
 			distance = Vector3.Distance(transform.position, player.transform.position);
 			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
-			if (Input.GetKeyDown(KeyCode.Mouse0) && distance < 2 && Physics.Raycast (ray, out hit) && hit.collider.gameObject.tag == "Door") {
-				gameObject.GetComponent<Animation>().Play ("DoorClose");
-				opened = false;
+			if (Input.GetKeyDown(KeyCode.Mouse0) && distance < 2 && Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "Door")
+			{
+				gameObject.GetComponent<Animation>().Play("DoorOpen");
+				StartCoroutine(DoorAnimOpen());
+			}
+		}		
+		else
+        {
+			distance = Vector3.Distance(transform.position, player.transform.position);
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Input.GetKeyDown(KeyCode.Mouse0) && distance < 2 && Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "Door")
+			{
+				gameObject.GetComponent<Animation>().Play("DoorClose");
+				StartCoroutine(DoorAnimClose());
 			}
 		}
+	}
+	IEnumerator DoorAnimOpen()
+    {
+			yield return new WaitForSeconds(2f);
+			opened = true;	
+	}
+	IEnumerator DoorAnimClose()
+	{
+			yield return new WaitForSeconds(2f);
+			opened = false;		
 	}
 }
